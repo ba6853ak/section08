2c6ed2042864f4b7aa9e25b5675043ed278ce95d
confidential <>
2025-05-05 23:09:51 +0900

    CREATE - 투두 추가하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./App.css";
 
 import Header from "./components/Header";
@@ -8,7 +8,7 @@ import List from "./components/List";
 function App() {
   const mockData = [
     {
-      id: 0,
+      id: 2,
       isDone: false,
       content: "React 공부하기",
       date: new Date().getTime(),
@@ -20,17 +20,30 @@ function App() {
       date: new Date().getTime(),
     },
     {
-      id: 2,
+      id: 0,
       isDone: false,
       content: "노래 연습하기",
       date: new Date().getTime(),
     },
   ];
   const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
+
   return (
     <div className="App">
       <Header></Header>
-      <Editor></Editor>
+      <Editor onCreate={onCreate}></Editor>
       <List></List>
     </div>
   );
